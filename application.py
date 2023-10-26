43a09504111361d9ae05774fa1064886f3ccc994
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2023-10-26 20:39:47 +0900

    Create application.py

---
+++ application.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import sys
+
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return render_template("index.html")
+
+@application.route("/list") 
+def view_list():
+    return render_template("list.html")
+
+@application.route("/review") 
+def view_review():
+    return render_template("review.html")
+
+@application.route("/reg_items") 
+def reg_item():
+    return render_template("reg_items.html")
+
+@application.route("/reg_reviews") 
+def reg_review():
+    return render_template("reg_reviews.html")
+
+@application.route("/submit_item")
+def reg_item_submit():
+    name = request.args.get("name")
+    seller = request.args.get("seller")
+    addr = request.args.get("addr")
+    email = request.args.get("email")
+    category = request.args.get("category")
+    card = request.args.get("card")
+    status = request.args.get("status")
+    phone = request.args.get("phone")
+
+    print(name,addr,tel,category,park,time,site)
+    #return render_template("reg_item.html")
+    
+@application.route("/submit_item_post", methods=['POST']) 
+def reg_item_submit_post():
+    print("11111111")
+    image_file = request.files["file"]
+    image_file.save("static/images/{}".format(image_file.filename))
+    data = request.form
+    print("2222")
+    return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
+
+if __name__== "__main__":
+    application.run(host='0.0.0.0', debug=True)
