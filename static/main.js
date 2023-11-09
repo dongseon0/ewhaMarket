54eba9e28d67294b25be2c77b38add428a0deddf
jungh150 <jungh150@naver.com>
2023-11-09 15:11:41 +0900

    마이페이지 추가
    마이페이지, 로그아웃 버튼이 보이도록 js 추가

--- static/main.js
+++ static/main.js
@@ -10,17 +10,17 @@ toggleButton.addEventListener('click', () => {
 });
 
 // 로그인 여부에 따라 네비바 버튼 바꾸기
-var isLogin = true;
-var loginBtn = document.getElementById("nav-login");
-var signupBtn = document.getElementById("nav-signup");
-loginBtn.addEventListener("click", login);
-signupBtn.addEventListener("click", login);
+// var isLogin = true;
+// var loginBtn = document.getElementById("nav-login");
+// var signupBtn = document.getElementById("nav-signup");
+// loginBtn.addEventListener("click", login);
+// signupBtn.addEventListener("click", login);
 
-function login() {
-    document.getElementById("nav-login").innerHTML="마이 페이지";
-    document.getElementById("nav-signup").innerHTML="로그아웃";
-}
-function logout() {
-    document.getElementById("nav-login").innerHTML="로그인";
-    document.getElementById("nav-signup").innerHTML="회원가입";
-}
+// function login() {
+//     document.getElementById("nav-login").innerHTML="마이 페이지";
+//     document.getElementById("nav-signup").innerHTML="로그아웃";
+// }
+// function logout() {
+//     document.getElementById("nav-login").innerHTML="로그인";
+//     document.getElementById("nav-signup").innerHTML="회원가입";
+// }
