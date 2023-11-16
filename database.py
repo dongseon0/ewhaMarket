c6097a145f5337bcb1e68d35e7feff51e94b6f1c
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2023-11-16 16:11:36 +0900

    add id and pythonpath change

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import json
 
 class DBhandler:
     def __init__(self):
-        with open('./authentication/firebase_auth.json') as f:
+        with open('ewhaMarket/authentication/firebase_auth.json') as f:
             config=json.load(f)
 
         firebase = pyrebase.initialize_app(config)
