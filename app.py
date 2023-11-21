b5de6b92d42ed198ea1fe2626019c09bdeeb716c
jungh150 <jungh150@naver.com>
2023-11-21 13:53:25 +0900

    함수 중복 오류 수정

--- app.py
+++ app.py
@@ -150,18 +150,6 @@ def personal():
     return render_template("my_info.html")
 
 
-@application.route("/signup_post", methods=['POST'])
-def register_user():
-    data = request.form
-    pw = request.form['pw']
-    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
-    if DB.insert_user(data, pw_hash):
-        return render_template("login.html")
-    else:
-        flash("user id already exist!")
-        return render_template("signup.html")
-
-
 @application.route("/dynamicurl/<varible_name>/")
 def DynamicUrl(variable_name):
     return str(variable_name)
