c27cfb620cc197ea9b594516d2b714273108cb26
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2023-11-21 12:39:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def view_item_detail(name):
     # get_item_byname 상품 이름으로 데이터 가져오는 함수 생성
     data = DB.get_item_byname(str(name))
     print("####data:", data)
-    return render_template("details_of_item.html", name=name, data=data, img_path="static/images/{}".format(image_file.filename))
+    return render_template("details_of_item.html", name=name, data=data)
 
 
 
