76a790aa39a04acef28ab350505f239e93727a5d
iioese <iioese109@gmail.com>
2023-11-23 12:14:03 +0900

    11/23
    브랜치 잘못해서 되돌려놓기/..

--- database.py
+++ database.py
@@ -10,14 +10,15 @@ class DBhandler:
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
 
-    def reg_review(self, data, img_path):
+    def insert_review(self, data, img_path):
         review_info = {
             "reviewTitle": data["reviewTitle"],
             "reviewContents": data["reviewContents"],
             "starsVariable": data["starsVariable"],
             "img_path": img_path
         }
-        self.db.child("review").child(data["productName"]).set(review_info)
+        self.db.child("review").child(data["reviewTitle"]).set(review_info)
+        print(data, img_path)
         return True
 
     def insert_item(self, name, data, img_path):
