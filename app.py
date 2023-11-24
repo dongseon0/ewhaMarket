938854e420c659a0f31f1b1e233d649c0b12eb7e
jungh150 <jungh150@naver.com>
2023-11-25 02:29:22 +0900

    로그인/회원가입/마이페이지 css 수정

--- app.py
+++ app.py
@@ -9,14 +9,16 @@ application.config["SECRET_KEY"] = "helloosp"
 DB = DBhandler()
 
 
+# 메인
 @application.route("/")
 def hello():
     # return render_template("index.html")
-    return redirect(url_for('view_list'))
+    return redirect(url_for('view_product_list'))
 
 
+# 상품
 @application.route("/product_list")
-def view_list():
+def view_product_list():
     # html에 페이지 인덱스 클릭할 때마다 get으로 받아옴
     page = request.args.get("page", 0, type=int)
     per_page = 12  # item count to display per page
@@ -25,7 +27,11 @@ def view_list():
     start_idx = per_page*page
     end_idx = per_page*(page+1)  # 페이지 인덱스로 start_idx, end_idx 생성
     data = DB.get_items()  # read the table
-    item_counts = len(data)
+    if data is None:
+        data = {}  # 또는 [] 등 비어있는 데이터로 초기화
+        item_counts = 0
+    else:
+        item_counts = len(data)
     # 한 페이지에 start_idx, end_idx 만큼 읽어오기
     data = dict(list(data.items())[start_idx:end_idx])
     tot_count = len(data)
@@ -51,8 +57,19 @@ def view_list():
     )
 
 
+@application.route("/auction_list")
+def view_auction_list():
+    return render_template("auction_list.html")
+
+
+@application.route("/list")
+def view_list():
+    return render_template("list.html")
+
+
 @application.route("/reg_item")
 def reg_item():
+<<<<<<< HEAD
     return render_template("reg_item.html")
 
 @application.route("/submit_item")
@@ -69,6 +86,13 @@ def reg_item_submit():
 
     # print(name,addr,tel,category,park,time,site)
     # return render_template("reg_item.html")
+=======
+    if session.get('id') is None:
+        flash("로그인하쇼")
+        return render_template("login.html")
+    else:
+        return render_template("reg_item.html")
+>>>>>>> 6dfef73f44874d831798cbc920a458c9a2c0486b
 
 
 @application.route("/submit_item_post", methods=['POST'])
@@ -76,10 +100,92 @@ def reg_item_submit_post():
     image_file = request.files["file"]
     image_file.save("static/images/{}".format(image_file.filename))
     data = request.form
-    DB.insert_item(data['name'], data, image_file.filename)
-    return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
+    data_key = DB.insert_item(data, image_file.filename, id=session.get('id'))
+    return redirect(url_for('view_details_of_item', key=data_key))
 
 
+@application.route("/view_detail/<key>/")
+def view_details_of_item(key):
+    # get_item_byname 상품 이름으로 데이터 가져오는 함수 생성
+    data = DB.get_item_bykey(str(key))
+    return render_template("details_of_item.html", key=key, data=data)
+
+
+# 리뷰
+@application.route("/reg_review/<id>/")
+def reg_review_init(id):
+    if session.get('id') is None:
+        flash("로그인하쇼")
+        return render_template("login.html")
+    else:
+        return render_template("reg_review.html", buyerId=session.get('id'), sellerId=id)
+
+
+@application.route("/reg_review", methods=['POST'])
+def reg_review():
+    image_file = request.files["file"]
+    image_file.save("static/images/{}".format(image_file.filename))
+    data = request.form
+    sellerId = data.get('sellerId')
+    review_key = DB.reg_review(
+        data, image_file.filename, buyerId=data.get('buyerId'), sellerId=sellerId)
+    return redirect(url_for('view_details_of_review', key=review_key, sellerId=sellerId))
+
+
+@application.route("/details_of_review/<sellerId>/<key>/")
+def view_details_of_review(key, sellerId):
+    data = DB.get_review_bykey(key, sellerId)
+    return render_template("details_of_review.html", data=data)
+
+
+@application.route("/user_reviews/<id>/")
+def view_user_reviews(id):
+    page = request.args.get("page", 0, type=int)
+    per_page = 5  # item count to display per page
+    per_row = 1  # item count to display per row
+    row_count = int(per_page/per_row)
+    start_idx = per_page*page
+    end_idx = per_page*(page+1)  # 페이지 인덱스로 start_idx, end_idx 생성
+    data = DB.get_reviews(id)  # read the table
+    if not data:
+        data = {}
+        item_counts = 0
+    else:
+        item_counts = len(data)
+
+    # 한 페이지에 start_idx, end_idx 만큼 읽어오기
+    data = dict(list(data.items())[start_idx:end_idx])
+    tot_count = len(data)
+    for i in range(row_count):  # last row
+        if (i == row_count-1) and (tot_count % per_row != 0):
+            locals()['data_{}'.format(i)] = dict(
+                list(data.items())[i*per_row:])
+        else:
+            locals()['data_{}'.format(i)] = dict(
+                list(data.items())[i*per_row:(i+1)*per_row])
+
+    return render_template(
+        "user_reviews.html",
+        datas=data.items(),
+        row1=locals()['data_0'].items(),
+        row2=locals()['data_1'].items(),
+        row3=locals()['data_2'].items(),
+        row4=locals()['data_3'].items(),
+        row5=locals()['data_4'].items(),
+        limit=per_page,
+        page=page,  # 현재 페이지 인덱스
+        page_count=int((item_counts/per_page) + 1),  # 페이지 개수
+        total=item_counts,
+        id=id
+    )
+
+
+@application.route("/user_list/<id>/")
+def view_user_list(id):
+    return render_template("user_list.html", id=id)
+
+
+# 로그인
 @application.route("/login")
 def login():
     return render_template("login.html")
@@ -87,12 +193,12 @@ def login():
 
 @application.route("/login_confirm", methods=['POST'])
 def login_user():
-    id_ = request.form['id']
+    id = request.form['id']
     pw = request.form['pw']
     pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
-    if DB.find_user(id_, pw_hash):
-        session['id'] = id_
-        return redirect(url_for('view_list'))
+    if DB.find_user(id, pw_hash):
+        session['id'] = id
+        return redirect(url_for('hello'))
     else:
         flash("Wrong ID or PW!")
         return render_template("login.html")
@@ -101,7 +207,7 @@ def login_user():
 @application.route("/logout")
 def logout_user():
     session.clear()
-    return redirect(url_for('view_list'))
+    return redirect(url_for('hello'))
 
 
 @application.route("/signup")
@@ -121,6 +227,7 @@ def register_user():
         return render_template("signup.html")
 
 
+# 마이페이지
 @application.route("/my_page")
 def mypage():
     return render_template("my_page.html")
@@ -141,10 +248,12 @@ def personal():
     return render_template("my_info.html")
 
 
+# 그외
 @application.route("/dynamicurl/<varible_name>/")
 def DynamicUrl(variable_name):
     return str(variable_name)
 
+<<<<<<< HEAD
 #상품/리뷰 상세보기 
 @application.route("/view_detail/<name>/")
 def view_item_detail(name):
@@ -217,6 +326,8 @@ def reg_review_submit_post():
     DB.insert_review(data['reviewTitle'], data, image_file.filename)
     return render_template("user_reviews.html", data=data, img_path="static/images/{}".format(image_file.filename))
 
+=======
+>>>>>>> 6dfef73f44874d831798cbc920a458c9a2c0486b
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -9,21 +9,34 @@ class DBhandler:
 
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
+    
 
+<<<<<<< HEAD
 #user db
     def insert_user(self, data, pw):
         user_info = {
             "id": data['id'],
+=======
+    # 로그인
+    def insert_user(self, data, pw):
+        user_info = {
+>>>>>>> 6dfef73f44874d831798cbc920a458c9a2c0486b
             "pw": pw,
             "nickname": data['nickname']
         }
         if self.user_duplicate_check(str(data['id'])):
+<<<<<<< HEAD
             self.db.child("user").push(user_info)
+=======
+            self.db.child("users").child(data['id']).child(
+                "user_info").set(user_info)
+>>>>>>> 6dfef73f44874d831798cbc920a458c9a2c0486b
             return True
         else:
             return False
 
     def user_duplicate_check(self, id_string):
+<<<<<<< HEAD
         users = self.db.child("user").get()
         if str(users.val()) == "None":  # first registration
             return True
@@ -42,6 +55,49 @@ class DBhandler:
             if value['id'] == id_ and value['pw'] == pw_:
                 return True
         return False
+=======
+        users = self.db.child("users").get()
+        if str(users.val()) == "None":  # 첫 회원가입
+            return True
+        else:
+            if id_string in users.val():
+                return False
+        return True
+
+    def find_user(self, id, pw):
+        users = self.db.child("users").get()
+        for user in users.each():
+            user_id = user.key()
+            if user_id == id:
+                user_data = user.val()
+                user_info = user_data.get("user_info")
+                if user_info.get("pw") == pw:
+                    return True
+                else:
+                    return False
+        return False
+    
+
+    # 상품
+    def insert_item(self, data, img_path, id):
+        item_info = {
+            "sellerId": id,
+            "name": data["name"],
+            "status": data["status"],
+            "description": data["description"],
+            "method": data["method"],
+            "location": data["location"],
+            "quantity": data["quantity"],
+            "category": data["category"],
+            "tag": data["tag"],
+            "phone": data["phone"],
+            "img_path": img_path
+        }
+        item_data = self.db.child("users").child(id).child("user_list").push(str(data["name"]))
+        item_key = item_data['name']
+        self.db.child("items").child(item_key).set(item_info)
+        return item_key
+>>>>>>> 6dfef73f44874d831798cbc920a458c9a2c0486b
 
 #상품db
     def insert_item(self, name, data, img_path):
@@ -62,28 +118,39 @@ class DBhandler:
         return True
 
     def get_items(self):
-        items = self.db.child("item").get().val()
+        items = self.db.child("items").get().val()
         return items
 
-    def get_item_byname(self, name):
-        items = self.db.child("item").get()
+    def get_item_bykey(self, key):
+        items = self.db.child("items").get()
         target_value = ""
         print("###########", name)
         for res in items.each():
             key_value = res.key()
 
-            if key_value == name:
+            if key_value == key:
                 target_value = res.val()
         return target_value
+<<<<<<< HEAD
 
 #리뷰db
     def insert_review(self, name, data, img_path):
         review_info = {
+=======
+    
+
+    # 리뷰
+    def reg_review(self, data, img_path, buyerId, sellerId):
+        review_info = {
+            "sellerId": sellerId,
+            "buyerId": buyerId,
+>>>>>>> 6dfef73f44874d831798cbc920a458c9a2c0486b
             "reviewTitle": data["reviewTitle"],
             "reviewContents": data["reviewContents"],
             "starsVariable": data["starsVariable"],
             "img_path": img_path
         }
+<<<<<<< HEAD
         self.db.child("review").child(name).set(review_info)
         return True
         
@@ -101,3 +168,22 @@ class DBhandler:
             if key_value == name:
                 target_value = res.val()
         return target_value
+=======
+        review_data = self.db.child("users").child(sellerId).child("user_reviews").push(review_info)
+        review_key = review_data['name']
+        return review_key
+
+    def get_review_bykey(self, key, sellerId):
+        reviews = self.db.child("users").child(sellerId).child("user_reviews").get()
+        target_value = ""
+        for res in reviews.each():
+            key_value = res.key()
+
+            if key_value == key:
+                target_value = res.val()
+        return target_value
+
+    def get_reviews(self, sellerId):
+        items = self.db.child("users").child(sellerId).child("user_reviews").get().val()
+        return items
+>>>>>>> 6dfef73f44874d831798cbc920a458c9a2c0486b
