9527345f410687b8a1275998a4292a32662cf386
jungh150 <jungh150@naver.com>
2023-12-04 23:44:06 +0900

    찜 날라간거 복구

--- database.py
+++ database.py
@@ -117,6 +117,23 @@ class DBhandler:
         return True
 
     # 찜한 상품 가져오기
+    def get_items_byheart(self, id):
+        items = self.db.child("users").child(id).child("user_wish").get()
+        target_value=[]
+        target_key=[]
+        for res in items.each():
+            value = res.val()
+            key_value = res.key()
+            if value['interested'] == "Y":
+                target_value.append(value)
+                target_key.append(key_value)
+        print("######target_value",target_value)
+        new_dict={}
+        for k,v in zip(target_key,target_value):
+            new_dict[k]=v
+        return new_dict 
+
+    # 카테고리 별로 상품 가져오기
     def get_items_bycategory(self, cate): 
         items = self.db.child("item").get() 
         target_value=[]
