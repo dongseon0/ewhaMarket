99babbbd2549a2882ecade4cae01b516f9ef7077
kjm3757 <kjm03070519@gmail.com>
2023-12-06 04:37:52 +0900

    reg_item.js checkInput() 완성

--- static/reg_item.js
+++ static/reg_item.js
@@ -7,22 +7,35 @@ function checkInput(){
     var description = document.getElementById("description").value;
     var quantity = document.getElementById("quantity").value;
     var selectPricingValue = $('input[name=select-pricing-button]:checked').val();
-    var name = document.getElementById("name");
-    var nameValue = name.value;
-    var location = document.getElementById("location");
-    var locationValue = location.value;
-    var phone = document.getElementById("phone");
-    var phoneValue = phone.value;
-    var fixedPrice = document.getElementById("fixedPrice");
-    var fixedPriceValue = fixedPrice.value;
-    var startPrice = document.getElementById("startPrice");
-    var startPriceValue = startPrice.value;
+    var name = document.getElementById("name").value;
+    var location = document.getElementById("location").value;
+    var phone = document.getElementById("phone").value;
+    var fixedPrice = document.getElementById("fixedPrice").value;
+    var startPrice = document.getElementById("startPrice").value;
+
+    var checkStartDate = document.getElementById("startDate").value;
+    var checkStartTime = document.getElementById("startTime").value;
+    var checkEndDate = document.getElementById("endDate").value;
+    var checkEndTime = document.getElementById("endTime").value;
+
+    var startDate = checkStartDate.split("-");
+    var startTime = checkStartTime.split(":");
+    var endDate = checkEndDate.split("-");
+    var endTime = checkEndTime.split(":");
+
+    var now = new Date();
+    var start = new Date(parseInt(startDate[0]), parseInt(startDate[1]) - 1, parseInt(startDate[2]), parseInt(startTime[0]), parseInt(startTime[1]));
+    var end = new Date(parseInt(endDate[0]), parseInt(endDate[1]) - 1, parseInt(endDate[2]), parseInt(endTime[0]), parseInt(endTime[1]));
+    var nt = now.getTime();
+    var st = start.getTime();
+    var et = end.getTime();
+
     var risingPrice = $('input:radio[name=select-rising-price]').is(':checked');
     var status = $('input:radio[name=select-status-button]').is(':checked');
     var method = $('input:radio[name=select-transaction-method-button]').is(':checked');
     var selectPricing = $('input:radio[name=select-pricing-button]').is(':checked');
 
-    if(!nameValue) {
+    if(!name) {
         alert("상품명을 입력해주세요.");
         return false;
     }
@@ -42,7 +55,7 @@ function checkInput(){
         return false;
     }
 
-    if(!locationValue) {
+    if(!location) {
         alert("선호 거래 지역을 입력해주세요.");
         return false;
     }
@@ -58,14 +71,14 @@ function checkInput(){
     }
 
     if(selectPricingValue == "고정가격") {
-        if(!fixedPriceValue) {
+        if(!fixedPrice) {
             alert("가격을 입력해주세요.");
             return false;
         }
     }
 
     else {
-        if(!startPriceValue) {
+        if(!startPrice) {
             alert("경매 시작가를 입력해주세요.");
             return false;
         }
@@ -75,9 +88,39 @@ function checkInput(){
             return false;
         }
 
+        if(!checkStartDate) {
+            alert("경매 시작 날짜를 설정해주세요.");
+            return false;
+        }
+
+        if(!checkStartTime) {
+            alert("경매 시작 시간을 설정해주세요.");
+            return false;
+        }
+
+        if(!checkEndDate) {
+            alert("경매 종료 날짜를 설정해주세요.");
+            return false;
+        }
+
+        if(!checkEndTime) {
+            alert("경매 종료 시간을 설정해주세요.");
+            return false;
+        }
+        
+        if(parseInt(st) < parseInt(nt)) {
+            alert("시작 시각을 현재 시각 후로 설정해주세요.");
+            return false;
+        }
+
+        if(parseInt(et) < parseInt(st)) {
+            alert("종료 시각을 시작 시각 후로 설정해주세요.");
+            return false;
+        }
+
     }
 
-    if(!phoneValue) {
+    if(!phone) {
         alert("연락처를 입력해주세요.");
         return false;
     }
