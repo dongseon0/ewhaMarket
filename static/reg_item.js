281fd83eba664d4049ad9ef85ba96704f7a3aba9
kjm3757 <kjm03070519@gmail.com>
2023-12-06 02:38:02 +0900

    reg_item.js checkInput()에 text타입, radio타입 추가

--- static/reg_item.js
+++ static/reg_item.js
@@ -5,10 +5,40 @@ function div_OnOff(myId, yourId) {
 
 function checkInput(){
     var name = document.getElementById("name");
-    var namevalue = name.value;
+    var nameValue = name.value;
+
+    var location = document.getElementById("location");
+    var locationValue = location.value;
+
+    var phone = document.getElementById("phone");
+    var phoneValue = phone.value;
     
-    if(!namevalue){
-        alert("상품명을 입력하세요.");
+    var status = $('input:radio[name=select-status-button]').is(':checked');
+    var method = $('input:radio[name=select-transaction-method-button]').is(':checked');
+
+    if(!nameValue) {
+        alert("상품명을 입력해주세요.");
         return false;
     }
+
+    if(!status) {
+        alert("상품 상태를 선택해주세요.")
+        return false;
+    }
+
+    if(!method) {
+        alert("거래 방식을 선택해주세요.")
+        return false;
+    }
+
+    if(!locationValue) {
+        alert("선호 거래 지역을 입력해주세요.");
+        return false;
+    }
+
+    if(!phoneValue) {
+        alert("연락처를 입력해주세요.");
+        return false;
+    }
+
 }
