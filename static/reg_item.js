1df02f54228d0903ba89184c0b6e6aecf638c88e
kjm3757 <kjm03070519@gmail.com>
2023-12-05 22:00:50 +0900

    reg_item 상품명 입력 없을 시 alert 띄우기

--- static/reg_item.js
+++ static/reg_item.js
@@ -1,5 +1,14 @@
 function div_OnOff(myId, yourId) {
-    
     document.getElementById(myId).style.display = "";
     document.getElementById(yourId).style.display = "none";
+}
+
+function checkInput(){
+    var name = document.getElementById("name");
+    var namevalue = name.value;
+    
+    if(!namevalue){
+        alert("상품명을 입력하세요.");
+        return false;
+    }
 }
